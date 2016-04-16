ef0e94675d486f91f55e442cb5c25bc2a07707de
Suhas Bhairav <suhasbhairav@gmail.com>
2016-04-16 13:39:21 +0200

    Small program

---
+++ basic2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main( int argc, char *argv){
+	
+	int rank, size;
+	MPI_Init(NULL, NULL);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	
+	printf("This is another program from process %d of %d\n",
+		rank, size);
+
+	MPI_Finalize();
+
+
+	return 0;
+}
+
