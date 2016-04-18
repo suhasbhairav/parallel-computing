df5100660892344d957bfca81dd765ac5a6313ef
Suhas Bhairav <suhasbhairav@gmail.com>
2016-04-18 09:37:20 +0200

    Simple print

---
+++ simpleprint.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char *argv){
+	MPI_Init(NULL, NULL);
+	int rank, size;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	
+	printf("Simple print from process %d of size %d\n", rank, size);
+
+
+	MPI_Finalize();
+
+	return 0;
+}
